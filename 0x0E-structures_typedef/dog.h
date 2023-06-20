c3a6e89edc000d8ac32576a9cb14c30b82b520ec
nekwueme2 <nekwueme2@gmail.com>
2023-06-19 21:12:10 -0700

    A struct dog data structure that has the details of a dog with three elements, compiled and running as expected

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a data struct that holds information about a dog
+ * @name: a pointer to character string
+ * @age: age of a dog with floating point value
+ * @owner: a pointer to character string represent the owner
+ *
+ * Description: This allows grouping these related data elements together.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
